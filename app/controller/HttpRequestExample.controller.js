563d673b0d948262235ef433704d07e6da9eade0
9banker <jirapan_yankhan@hotmail.com>
2018-10-12 09:35:01 +0700

    [ EDIT ] add function request

---
+++ app/controller/HttpRequestExample.controller.js
@@ -0,0 +1,16 @@
+/**
+ * @author llUJO IT23
+ * @version 1.0
+ */
+
+ import request from 'request';
+
+ const HttpRequestExample = {
+
+    getRequest:async(req,res) => {
+
+        console.log(req);
+
+    }
+
+ }
